5c1bb9c096fa586bbded4e9f56a0358c19bd0a2e
udonrm <b201395@hiroshima-u.ac.jp>
2024-02-09 00:26:47 +0900

    SpotifyAPIの設定

---
+++ src/app/api/spotify/route.ts
@@ -0,0 +1,32 @@
+const client_id = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
+const client_secret = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET;
+
+export const getToken = async () => {
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    body: new URLSearchParams({
+      grant_type: "client_credentials",
+    }),
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " +
+        Buffer.from(client_id + ":" + client_secret).toString("base64"),
+    },
+  });
+  const data = await response.json();
+  return await data.access_token;
+};
+
+export const searchTracks = async (access_token: string, query: string) => {
+  const response = await fetch(
+    `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+      query
+    )}&type=track`,
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + access_token },
+    }
+  );
+  return await response.json();
+};

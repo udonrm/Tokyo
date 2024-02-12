69908369360876a25835bfb63a277ed0f6c4d3cd
udonrm <b201395@hiroshima-u.ac.jp>
2024-02-12 18:03:23 +0900

    ローディングアニメーションの実装

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className=" flex justify-center items-center h-screen bg-neutral-950 text-white text-7xl animate-fade-out ">
+      TOKYO
+    </div>
+  );
+};
+
+export default Loading;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,14 +73,33 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "fade-out": {
+          "0%": {
+            opacity: "1",
+          },
+          to: {
+            opacity: "0",
+          },
+        },
+        "text-blur-out": {
+          "0%": {
+            filter: "blur(.01)",
+          },
+          to: {
+            filter: "blur(12px) opacity(0%)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "fade-out": "fade-out 4s ease   both",
+        "text-blur-out":
+          "text-blur-out 2s cubic-bezier(0.550, 0.085, 0.680, 0.530)   both",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("tailwindcss-animated")],
 } satisfies Config;
 
 export default config;

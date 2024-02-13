689575db1ba591ec6b61dd92af0fac547d37642b
udonrm <b201395@hiroshima-u.ac.jp>
2024-02-13 16:00:28 +0900

    背景動画のサイズ調整

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
-    <div className=" flex justify-center items-center h-screen bg-neutral-950 text-white text-7xl animate-fade-out ">
-      TOKYO
+    <div className="flex justify-center items-center h-screen bg-neutral-950 text-white text-7xl animate-fade-out">
+      <p>TOKYO</p>
     </div>
   );
 };
